97537a1742e929c4e0e863c6900fb77171d56502
deh0017 <deh0017@auburn.edu>
2017-05-22 09:01:34 -0500

    Update Entity.java

--- resources/Entity.java
+++ resources/Entity.java
@@ -1,3 +1,5 @@
+import java.io.*
+
 public class Entity{
   //ALL Entities have:
   private String name;
@@ -18,4 +20,25 @@ public class Entity{
   //Only PLAYER Entities have:
   //private int totalMP;
   //private int currentMP;
+  
+  
+  public Entity(String newName){
+    String filename = "GC/";
+    filename.concat(newName);
+    filename.concat(".txt");
+    
+    String line = null;
+    BufferedReader bufferedReader = null;
+    
+    try{
+      FileReader fileReader = new FileReader(filename);
+      bufferedReader = new bufferedReader(fileReader);
+      
+      line = bufferedReader.readline();
+      name = line;
+      
+      line = bufferedReader.readline();
+      totalHP 
+    }
+  }
 }
