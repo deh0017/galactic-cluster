b4bdf2639eff43c762d9737c47bb3c0562da09b8
deh0017 <deh0017@auburn.edu>
2017-05-22 08:47:46 -0500

    Create PlayerEntity.java

---
+++ resources/PlayerEntity.java
@@ -0,0 +1,23 @@
+public class PlayerEntity extends MajorEntity{
+  //ALL Entities have:
+  /*
+  private String name;
+  private int totalHP;
+  private int currentHP;
+  
+  private int atk;
+  private int def;
+  
+  //private Status status;
+  
+  //private Attribute[] weaknesses;
+  //private Attribute[] resistances;
+  
+  //Only MAJOR Entities have:
+  //private Spell[] spells;
+  */
+  //Only PLAYER Entities have:
+  private int totalMP;
+  private int currentMP;
+  private boolean usesMP;
+}
